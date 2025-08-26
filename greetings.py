94e94dce1b13dafe8ef735ad523a530140b579d6
amanpreetbhogal66-preet <amanpreetbhogal66@gmail.com>
2025-08-26 17:34:04 +0530

    Basic Python Concepts

---
+++ greetings.py
@@ -0,0 +1,4 @@
+first = input("Enter the first name: ")
+last = input("Enter the last name: ")
+Fullname = first + last
+print(f"Hello, {Fullname}! Welcome To Python World.")
---
+++ operation.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
